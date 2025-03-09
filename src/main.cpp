64922d2391a6ec8a3682494f52ecc32656d9359b
fruityloops1 <fruityloops@littun.co>
2025-03-10 00:45:49 +0100

    Capten

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,39 @@
-#include "hk/gfx/DebugRenderer.h"
+#include "hk/diag/diag.h"
 #include "hk/hook/Trampoline.h"
 
-#include "agl/common/aglDrawContext.h"
+namespace al {
+    bool isPadHoldA(int port = -1);
+}
 
-#include "game/Sequence/HakoniwaSequence.h"
-#include "game/System/Application.h"
-#include "game/System/GameSystem.h"
+namespace sead {
+    template <typename T>
+    struct Vector3 {
+        T x, y, z;
+    };
 
-#include "al/Library/Controller/InputFunction.h"
-#include "al/Library/LiveActor/ActorPoseKeeper.h"
-#include "al/Library/LiveActor/ActorPoseUtil.h"
-#include "al/Library/LiveActor/LiveActor.h"
-#include "al/Library/LiveActor/LiveActorKit.h"
-#include "al/Library/Player/PlayerHolder.h"
-#include "al/Library/Scene/Scene.h"
-#include "al/Library/System/GameSystemInfo.h"
+    using Vector3f = Vector3<f32>;
+}
 
-HkTrampoline<void, al::LiveActor*> marioControl = hk::hook::trampoline([](al::LiveActor* player) -> void {
-    if (al::isPadHoldA(-1)) {
-        player->getPoseKeeper()->getVelocityPtr()->y = 0;
-        al::getTransPtr(player)->y += 20;
+struct PlayerActor {
+    u8 _0[0xB8];
+    struct Player {
+        struct PlayerProperty {
+            sead::Vector3f mTrans;
+            sead::Vector3f mFront;
+            sead::Vector3f mUp;
+            sead::Vector3f mVelocity;
+        }* mProperty;
+    }* mPlayer;
+};
+
+HkTrampoline<void, PlayerActor*> control = hk::hook::trampoline([](PlayerActor* player) -> void {
+    control.orig(player);
+    if (al::isPadHoldA()) {
+        player->mPlayer->mProperty->mTrans.y += 30.0f;
+        player->mPlayer->mProperty->mVelocity.y = 0;
     }
-
-    marioControl.orig(player);
-});
-
-HkTrampoline<void, GameSystem*> drawMainHook = hk::hook::trampoline([](GameSystem* gameSystem) -> void {
-    drawMainHook.orig(gameSystem);
-
-    auto* drawContext = Application::instance()->mDrawSystemInfo->drawContext;
-
-    HakoniwaSequence* sequence = static_cast<HakoniwaSequence*>(gameSystem->mSequence);
-    if (sequence == nullptr)
-        return;
-
-    al::Scene* scene = sequence->mCurrentScene;
-    al::LiveActor* player = nullptr;
-    if (scene && scene->mLiveActorKit && scene->mLiveActorKit->mPlayerHolder)
-        player = scene->mLiveActorKit->mPlayerHolder->tryGetPlayer(0);
-
-    auto* renderer = hk::gfx::DebugRenderer::instance();
-
-    renderer->clear();
-    renderer->begin(drawContext->getCommandBuffer()->ToData()->pNvnCommandBuffer);
-
-    renderer->setGlyphSize(0.45);
-
-    renderer->drawQuad(
-        { { 30, 30 }, { 0, 0 }, 0xef000000 },
-        { { 300, 30 }, { 1.0, 0 }, 0xef000000 },
-        { { 300, 100 }, { 1.0, 1.0 }, 0xef000000 },
-        { { 30, 100 }, { 0, 1.0 }, 0xef000000 });
-
-    renderer->setCursor({ 50, 50 });
-
-    if (player) {
-        const sead::Vector3f& trans = al::getTrans(player);
-
-        renderer->printf("Pos: %.2f %.2f %.2f\n", trans.x, trans.y, trans.z);
-    } else {
-        renderer->printf("No player\n");
-    }
-
-    renderer->end();
 });
 
 extern "C" void hkMain() {
-    marioControl.installAtSym<"_ZN19PlayerActorHakoniwa7controlEv">();
-    drawMainHook.installAtSym<"_ZN10GameSystem8drawMainEv">();
-    hk::gfx::DebugRenderer::instance()->installHooks();
+    control.installAtSym<"_ZN11PlayerActor7controlEv">();
 }
